866e318709325b532cf3b90ac04387084ef2fe20
Austin <austinlasseter@gmail.com>
2022-06-13 19:19:11 +0000

    update shell script again and add plotly

--- aws_studio_setup.sh
+++ aws_studio_setup.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-pip install plotly
-pip install dash
-pip install ipywidgets
+conda install -c conda-forge nodejs -y
+conda install -c conda-forge plotly -y
+conda install -c conda-forge dash -y
+conda install -c conda-forge ipywidgets -y
 
 export NODE_OPTIONS=--max-old-space-size=4096
 jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1 --no-build
 jupyter labextension install jupyterlab-plotly --no-build
 jupyter labextension install plotlywidget --no-build
 jupyter lab build
-unset NODE_OPTIONS
+unset NODE_OPTIONS 
