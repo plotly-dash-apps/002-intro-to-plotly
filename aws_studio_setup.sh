47bc552606b9cbb8377c9e37c192e59ac370654b
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2022-08-10 10:04:03 -0400

    Update aws_studio_setup.sh

--- aws_studio_setup.sh
+++ aws_studio_setup.sh
@@ -6,7 +6,7 @@ conda install -c conda-forge dash -y
 conda install -c conda-forge ipywidgets -y
 
 export NODE_OPTIONS=--max-old-space-size=4096
-jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1 --no-build
+jupyter labextension install @jupyter-widgets/jupyterlab-manager@3.1 --no-build
 jupyter labextension install jupyterlab-plotly --no-build
 jupyter labextension install plotlywidget --no-build
 jupyter lab build
